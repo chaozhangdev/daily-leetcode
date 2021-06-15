256d78c673b367391eb2e118399461dd60361bb3
Chao Zhang <raidenchao@gmail.com>
2021-06-15 10:32:00 -0700

    string

--- 1249.js
+++ 1249.js
@@ -3,14 +3,17 @@
  * @return {string}
  */
 var minRemoveToMakeValid = function (s) {
-  let result = [];
-  let counter = 0;
-
-  let c = s.charAt(i);
-
+  let stack = [];
+  let arr = Array.from(s);
   for (let i = 0; i < s.length; i++) {
-
- 
+    if (s.charAt(i) === "(") {
+      stack.push(i);
+    } else if (s.charAt(i) === ")") {
+      stack.length === 0 ? (arr[i] = "") : stack.pop();
+    }
   }
-  return result;
+  stack.forEach((index) => (arr[index] = ""));
+  return arr.join("");
 };
+
+console.log(minRemoveToMakeValid("lee(t(c)o)de)"));
